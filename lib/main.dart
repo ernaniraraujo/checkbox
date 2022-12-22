060d7c1090c7d1cad9f3909180567aa8e642ea03
Ernani Araujo <ernani_araujo@hotmail.com>
2022-12-22 11:39:07 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:checkbox/CampoTexto.dart';
+import 'package:checkbox/EntradaCheckbox.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      //home: CampoTexto(),
+      home: EntradaCheckbox(),
+    )
+  );
+}
+
